6b1f5b9140a664d9247bdf062be0b7b1a27cc434
jeevan6veeru <jeevan6veeru@gmail.com>
2025-09-15 17:07:24 +0530

    allure report added

--- tests/UI/uploaddownload.spec.js
+++ tests/UI/uploaddownload.spec.js
@@ -1,6 +1,6 @@
-import {ExcelJs} from 'exceljs';
-import { test, expect } from '@playwright/test';
- 
+const ExcelJs = require('exceljs');
+const { test, expect } = require('@playwright/test');
+
 async function writeExcelTest(searchText,replaceText,change,filePath)
 {
     
@@ -8,14 +8,14 @@ async function writeExcelTest(searchText,replaceText,change,filePath)
   await workbook.xlsx.readFile(filePath);
   const worksheet = workbook.getWorksheet('Sheet1');
   const output= await readExcel(worksheet,searchText);
- 
+
   const cell = worksheet.getCell(output.row,output.column+change.colChange);
   cell.value = replaceText;
   await workbook.xlsx.writeFile(filePath);
- 
+
 }
- 
- 
+
+
 async function readExcel(worksheet,searchText)
 {
     let output = {row:-1,column:-1};
@@ -45,12 +45,11 @@ test('Upload download excel validation',async ({page})=>
   const downloadPromise = page.waitForEvent('download');
   await page.getByRole('button',{name:'Download'}).click();
   await downloadPromise;
-  writeExcelTest(textSearch,updateValue,{rowChange:0,colChange:2},"/Users/rahulshetty/downloads/download.xlsx");
+  writeExcelTest(textSearch,updateValue,{rowChange:0,colChange:2},"C:/Users/Jeevan Ravi Kumar J/Downloads/download.xlsx");
   await page.locator("#fileinput").click();
-  await page.locator("#fileinput").setInputFiles("/Users/rahulshetty/downloads/download.xlsx");
+  await page.locator("#fileinput").setInputFiles("C:/Users/Jeevan Ravi Kumar J/Downloads/download.xlsx");
   const textlocator = page.getByText(textSearch);
   const desiredRow = await page.getByRole('row').filter({has :textlocator });
   await expect(desiredRow.locator("#cell-4-undefined")).toContainText(updateValue);
- 
- 
+
 })
